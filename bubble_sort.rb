6a30745bc0c4d5d3d008fac8a5fc1bf4b6b85b88
Clayton Siby <claytonsiby@gmail.com>
2020-07-03 18:59:49 +0200

    fix code errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,38 +1,38 @@
 def bubble_sort(arr)
-    unless arr.is_a?Array
-        puts "The argument is not an array." 
-    else
-      loop do
-        so_rt = false
-        (arr.length - 1).times do |i|
-           if  arr[i] > arr[i + 1]
-              arr[i], arr[i + 1] = arr[i + 1], arr[i]
-              so_rt = true
-           end
-       end
-       break if !(so_rt)
+  if arr.is_a? Array
+    loop do
+      so_rt = false
+      (arr.length - 1).times do |i|
+        if arr[i] > arr[i + 1]
+          arr[i], arr[i + 1] = arr[i + 1], arr[i]
+          so_rt = true
+        end
       end
-      arr
+      break unless so_rt
     end
+    arr
+  else
+    puts 'The argument is not an array.'
+  end
 end
 
-#This is the bubble_sort_by method.
+# This is the bubble_sort_by method.
 
 def bubble_sort_by(array)
-  unless array.is_a?Array
-    puts "The argument is not an array."
-  else
+  if array.is_a? Array
     loop do
       so_rt = false
 
       (array.length - 1).times do |i|
-        if (yield array[i] , array[i + 1]) > 0
+        if (yield array[i], array[i + 1]).positive?
           array[i], array[i + 1] = array[i + 1], array[i]
           so_rt = true
         end
       end
-      break if !(so_rt)
+      break unless so_rt
     end
     array
+  else
+    puts 'The argument is not an array.'
   end
 end
