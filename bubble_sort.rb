76aae7ecff09612fe81340a9c3b85956cdfa1712
RNtaate <royhyde14@gmail.com>
2020-07-03 18:07:47 +0300

    add bubble_sort_by method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -15,3 +15,24 @@ def bubble_sort(arr)
       arr
     end
 end
+
+#This is the bubble_sort_by method.
+
+def bubble_sort_by(array)
+  unless array.is_a?Array
+    puts "The argument is not an array."
+  else
+    loop do
+      so_rt = false
+
+      (array.length - 1).times do |i|
+        if (yield array[i] , array[i + 1]) > 0
+          array[i], array[i + 1] = array[i + 1], array[i]
+          so_rt = true
+        end
+      end
+      break if !(so_rt)
+    end
+    array
+  end
+end
